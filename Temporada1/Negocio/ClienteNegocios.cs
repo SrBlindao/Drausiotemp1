b4e59041da29ecca0e69dfcb93cb0d3b8f7df4fe
SrBlindao <erik_erikzinho@hotmail.com>
2023-10-15 19:46:59 -0300

    att

---
+++ Temporada1/Negocio/ClienteNegocios.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcessoBancoDados;
+using ObjetoTransferencia;
+using System.Data;
+
+namespace Negocio
+{
+    public class ClienteNegocios
+    {
+        //Instanciar = Criar um novo objeto baseado em um modelo
+        AcessoDadosSqlServer acessoDadosSqlServer = new AcessoDadosSqlServer();
+
+        public string Inserir(Cliente cliente)
+        {
+            try
+            {
+                acessoDadosSqlServer.LimparParamentros();
+                acessoDadosSqlServer.AdicionarParamentros("@Nome", cliente.Nome);
+                acessoDadosSqlServer.AdicionarParamentros("DataNascimento", cliente.DataNascimento);
+                acessoDadosSqlServer.AdicionarParamentros("@Sexo", cliente.Sexo);
+                acessoDadosSqlServer.AdicionarParamentros("@LimiteCompra", cliente.LimiteCompra);
+                string idCliente = acessoDadosSqlServer.ExecutarManipulacao(CommandType.StoredProcedure, "uspClienteInserir").ToString();
+                return idCliente;
+            }
+            catch (Exception excepetion)
+            {
+
+                return excepetion.Message;
+            }
+            
+        }
+    }
+}
---
+++ Temporada1/ObjetoTransferencia/Cliente.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjetoTransferencia
+{
+    public class Cliente
+    {
+        /*METODO GERAL E COMUM OUTRAS LINGUAGENS
+        //Variavel 
+        private int idCliente;
+
+        //GET = Pegar
+        public int getidCliente()
+        {
+            return idCliente;
+        }
+
+        //SET = SETAR
+        public void setIdCliente (int idClienteEnviado)
+        {
+            idCliente = idClienteEnviado;
+        }*/
+
+        //Metodo DOTNET
+        //PROP = ATALHO
+
+        public int idCliente { get; set; }
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public Boolean Sexo { get; set; }
+        public decimal LimiteCompra { get; set; }
+    }
+}
